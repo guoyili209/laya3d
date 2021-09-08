6c64d63c6c6760b68398b4aaccb436b47dcb16f1
guoyili209 <guoyili209@gmail.com>
2021-09-08 18:34:12 +0800

    update

--- pro1/src/Managers/SceneManager.ts
+++ pro1/src/Managers/SceneManager.ts
@@ -1,21 +1,16 @@
 import MeshSprite3D = Laya.MeshSprite3D;
 import Camera = Laya.Camera;
-export class SceneManager extends Laya.Script3D {
-    constructor() { super(); }
-    // private static _instance: SceneManager;
-    // static get Instance(): SceneManager {
-    //     if (this._instance == null) {
-    //         this._instance = new SceneManager();
-    //     }
-    //     return this._instance;
-    // }
-    public static Instance: SceneManager;
+export class SceneManager {
+    constructor() { }
+    private static _instance: SceneManager;
+    static get Instance(): SceneManager {
+        if (this._instance == null) {
+            this._instance = new SceneManager();
+        }
+        return this._instance;
+    }
     public player: MeshSprite3D;
     public camera: Camera;
-    onAwake() {
-        SceneManager.Instance = this;
-    }
-    onStart() {
-        console.log("SceneMgr start");
-    }
+    
+    
 }
